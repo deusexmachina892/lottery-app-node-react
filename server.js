c2586d9f3b1ef4667628c8f8a06c063c527afdec
deusexmachina892 <deusexmachina892@gmail.com>
2018-08-31 07:39:41 -0400

    Added a model and added functionalities to db and server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {save_user_information} = require('./models/server_db');
 const app = express();
 
 
@@ -8,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     var email = req.body.email;
     var amount = req.body.amount;
 
@@ -18,8 +19,12 @@ app.post('/', (req, res) => {
         return_info.message = "Amount should be greater than 1";
         return res.send(return_info);
     }
-
-    res.send({"email" : email, "amount": amount});
+    try{
+        var result =  await save_user_information({"email": email,"amount": amount});
+        res.send(result);
+    } catch(err){
+        res.status(400).send('Could not save user info');
+    }
 });
 
 const port = process.env.PORT || 3001
