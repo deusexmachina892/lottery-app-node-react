e697c05d95d37f227a11305cc6832b07607e2364
deusexmachina892 <deusexmachina892@gmail.com>
2018-09-02 00:13:04 -0400

    Added functionality to UI

--- public/index.js
+++ public/index.js
@@ -2,8 +2,21 @@ class App extends React.Component{
     constructor(props){
         super(props);
         this.state ={
-            'total_amount' : 1000   
-        }
+            'total_amount' : 1000,
+            'amount': '',
+            'email' : ''
+         }   
+    }
+
+    onSubmit = async (event)=>{
+        //prevent page from reloading
+        event.preventDefault();
+        const response = await axios.post('/post_info',{
+            email: this.state.email,
+            amount : this.state.amount 
+        });
+        console.log(response);
+
     }
     render(){
         return(<div>
@@ -11,10 +24,12 @@ class App extends React.Component{
             <div>
                 <p>Total Lottery Amount is {this.state.total_amount}</p>
             </div>
-            <form>
-                <input placeholder="email" />
-                <input placeholder="amount" />
-                <button>Participate</button>
+            <form onSubmit={this.onSubmit}>
+                <input placeholder="email" value={this.state.email} 
+                onChange={event => this.setState({email: event.target.value})}/>
+                <input placeholder="amount"
+                onChange={event => this.setState({amount: event.target.value})} />
+                <button type="submit">Participate</button>
             </form>
         </div>)
     }
